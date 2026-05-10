4a898d0cc4085cecea7007209f38efe351dd9997
namjuyeon <skawndus8@gmail.com>
2026-05-10 21:01:26 +0900

    project first update

---
+++ src/main/java/com/global/taxreturnbackendapi/TaxReturnBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.global.taxreturnbackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxReturnBackendApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxReturnBackendApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/global/taxreturnbackendapi/TaxReturnBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.global.taxreturnbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxReturnBackendApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
